161e9882a51e010d891a933cfb271c8ee6eef5f3
Brucy666 <robleach16@icloud.com>
2025-07-18 17:50:06 +0100

    Update sniper_loop.py

--- sniper_loop.py
+++ sniper_loop.py
@@ -1,6 +1,7 @@
 # sniper_loop.py
 import time
 from btc_sniper_engine import run_btc_sniper
+from bybit_sniper_engine import run_bybit_sniper
 from dotenv import load_dotenv
 import os
 
@@ -9,5 +10,10 @@ load_dotenv()
 print("[LOOP] Starting sniper loop...")
 
 while True:
+    print("[LOOP] Running KuCoin sniper...")
     run_btc_sniper()
+
+    print("[LOOP] Running Bybit sniper...")
+    run_bybit_sniper()
+
     time.sleep(60)  # Run every 60 seconds
